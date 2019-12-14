e856a9adb22960bb28fadfc48926ec9b7371eb4a
HyphnKnight <rafe.architect@gmail.com>
2019-12-14 02:48:47 -0500

    add Terrain

---
+++ src/entities/Terrain.ts
@@ -0,0 +1,33 @@
+import { DistrictDataId } from "./District";
+
+export const enum TerrainId {
+  Plain,
+  Forest,
+}
+
+export interface Terrain {
+  readonly id: TerrainId;
+  // 2 means it takes twice as long.
+  readonly movementModifier: number;
+  readonly allowableDistricts?: DistrictDataId[];
+}
+
+export const terrainTypes: { readonly [Id in TerrainId]: Terrain } = {
+  [TerrainId.Plain]: {
+    id: TerrainId.Plain,
+    movementModifier: 1,
+    allowableDistricts: [
+      DistrictDataId.Bakery,
+      DistrictDataId.Mill,
+      DistrictDataId.WheatFarm,
+    ],
+  },
+  [TerrainId.Forest]: {
+    id: TerrainId.Forest,
+    movementModifier: 1.5,
+    allowableDistricts: [
+      DistrictDataId.LumberCamp,
+      DistrictDataId.HuntingCamp,
+    ],
+  },
+}; 
